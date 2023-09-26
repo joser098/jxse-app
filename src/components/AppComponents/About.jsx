f16b084333d94a7a766935c5374c7e4e1664bf0c
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-26 00:55:03 -0300

    feat:Speech component created && About completed

--- src/components/AppComponents/About.jsx
+++ src/components/AppComponents/About.jsx
@@ -1,7 +1,27 @@
-const About = () => {
+import Profile from "./Profile";
+import Skills from "./Skills";
+import Speech from "./Speech";
+
+const getProfile = async () => {
+  const res = await fetch("https://server-nglt-dev.fl0.io/jxse/profile");
+  const data = await res.json();
+
+  return data;
+};
+
+const About = async () => {
+  const [profile] = await getProfile();
+  const { skills, about } = await profile;
+
   return (
-    <section className=" h-[calc(100vh-90px)]">
-      <h2>About</h2>
+    <section className="h-[calc(100vh-90px)] flex flex-col px-8 md:flex-row md:justify-around gap-5">
+      <div className="">
+        <Profile profile={profile} />
+
+        <Skills skills={skills} />
+      </div>
+
+      <Speech about={about} />
     </section>
   );
 };
---
+++ src/components/AppComponents/Speech.jsx
@@ -0,0 +1,192 @@
+const Speech = ({ about }) => {
+  return (
+    <div className="border-2 bg-black border-3 rounded-lg px-3 h-full mb-3 overflow-auto md:h-auto md:max-w-sm lg:max-w-xl xl:max-w-2xl">
+      <div className="flex flex-col items-center gap-2 py-2">
+        <svg
+          className="my-3"
+          width="64px"
+          height="64px"
+          viewBox="0 0 512 512"
+          fill="#ffffff"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              fill="var(--ci-primary-color, #fff)"
+              d="M425.39,200.035A184.3,184.3,0,0,0,290.812,91.289L317.568,48.48,290.432,31.52,255.127,88.008A184.046,184.046,0,0,0,86.61,200.035a71.978,71.978,0,0,0,0,143.93,184.071,184.071,0,0,0,338.78,0,71.978,71.978,0,0,0,0-143.93Zm27.152,99.975a39.77,39.77,0,0,1-27.76,11.961l-20.725.394-8.113,19.074a152.066,152.066,0,0,1-279.887,0l-8.114-19.074-20.725-.394a39.978,39.978,0,0,1,0-79.942l20.725-.394,8.114-19.074a152.067,152.067,0,0,1,279.887,0l8.113,19.074,20.725.394a39.974,39.974,0,0,1,27.76,67.981Z"
+              class="ci-primary"
+            ></path>{" "}
+            <rect
+              width="40"
+              height="40"
+              x="168"
+              y="232"
+              fill="var(--ci-primary-color, #fff)"
+              class="ci-primary"
+            ></rect>{" "}
+            <rect
+              width="40"
+              height="40"
+              x="304"
+              y="232"
+              fill="var(--ci-primary-color, #fff)"
+              class="ci-primary"
+            ></rect>{" "}
+            <path
+              fill="var(--ci-primary-color, #fff)"
+              d="M256,384a80,80,0,0,0,80-80H176A80,80,0,0,0,256,384Z"
+            ></path>{" "}
+          </g>
+        </svg>
+        <p className="text-sm md:text-base">{about.p1}</p>
+      </div>
+
+      <div className="flex flex-col items-center gap-2 py-2">
+        <svg
+          className="my-3"
+          fill="#fff"
+          height="64px"
+          width="64px"
+          version="1.1"
+          id="Layer_1"
+          viewBox="0 0 512 512"
+          stroke="#fff"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <polygon points="0,158.694 0,219.615 110.641,219.615 204.562,158.694 "></polygon>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <polygon points="307.438,158.694 401.359,219.615 512,219.615 512,158.694 "></polygon>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <polygon points="240.779,26.873 240.779,135.204 256,125.331 271.221,135.204 271.221,111.033 369.875,74.578 "></polygon>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M367.034,364.262c-8.051,0-14.6,6.549-14.6,14.6v106.264h29.2V378.861C381.633,370.811,375.084,364.262,367.034,364.262z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M144.966,364.262c-8.051,0-14.6,6.549-14.6,14.6v106.264h29.2V378.861C159.567,370.811,153.017,364.262,144.966,364.262z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M256,364.262c-8.05,0-14.599,6.549-14.599,14.599v106.265h29.199V378.861C270.599,370.811,264.05,364.262,256,364.262z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M256,243.015c-9.427,0-17.097,7.669-17.097,17.097s7.67,17.097,17.097,17.097c9.427,0,17.097-7.67,17.097-17.097 S265.427,243.015,256,243.015z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M256,161.616l-125.633,81.49v93.155c4.582-1.575,9.49-2.443,14.599-2.443c24.837,0,45.043,20.206,45.043,45.043v106.264 h20.949V378.86c0-24.836,20.206-45.042,45.042-45.042s45.042,20.206,45.042,45.042v106.265h20.949V378.861 c0-24.837,20.206-45.043,45.042-45.043c5.109,0,10.018,0.868,14.6,2.443v-93.155L256,161.616z M256,307.651 c-26.213,0-47.539-21.326-47.539-47.539s21.326-47.539,47.539-47.539c26.213,0,47.539,21.326,47.539,47.539 C303.539,286.325,282.213,307.651,256,307.651z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M0,250.058v235.068h99.924V378.861V250.058H0z M65.183,440.968H34.74v-33.69h30.443V440.968z M65.183,384.436H34.74 v-33.69h30.443V384.436z M65.183,327.905H34.74v-33.69h30.443V327.905z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+            <g>
+              {" "}
+              <g>
+                {" "}
+                <path d="M412.076,250.058v128.803v106.264H512V250.058H412.076z M477.259,440.968h-30.443v-33.69h30.443V440.968z M477.259,384.436h-30.443v-33.69h30.443V384.436z M477.259,327.905h-30.443v-33.69h30.443V327.905z"></path>{" "}
+              </g>{" "}
+            </g>{" "}
+          </g>
+        </svg>
+        <p className="text-sm md:text-base">{about.p2}</p>
+      </div>
+
+      <div className="flex flex-col items-center gap-2 py-2">
+        <svg
+          className="my-3"
+          width="64px"
+          height="64px"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M15.5 9L15.6716 9.17157C17.0049 10.5049 17.6716 11.1716 17.6716 12C17.6716 12.8284 17.0049 13.4951 15.6716 14.8284L15.5 15"
+              stroke="#fff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+            <path
+              d="M13.2942 7.17041L12.0001 12L10.706 16.8297"
+              stroke="#fff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+            <path
+              d="M8.49994 9L8.32837 9.17157C6.99504 10.5049 6.32837 11.1716 6.32837 12C6.32837 12.8284 6.99504 13.4951 8.32837 14.8284L8.49994 15"
+              stroke="#fff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+            <path
+              d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
+              stroke="#fff"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+            ></path>{" "}
+          </g>
+        </svg>
+        <p className="text-sm md:text-base">{about.p3}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Speech;
