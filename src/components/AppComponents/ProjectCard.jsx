df7900523f9605e13548e328f89eb4e80b1f9878
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-29 22:24:46 -0300

    feat: projects MVP done

--- src/components/AppComponents/ProjectCard.jsx
+++ src/components/AppComponents/ProjectCard.jsx
@@ -1,24 +1,38 @@
-const ProjectCard = ({ project }) => {
+const ProjectCard = ({ project, index, current }) => {
   return (
-    <div className="min-w-fit bg-fa rounded-lg mt-3">
-      <img className="rounded-lg" src={project.image} alt={project.title} />
+    <div
+      className="min-w-fit bg-fa rounded-lg mt-3 mx-5 transition-transform ease-out duration-500 flex flex-col justify-between"
+      style={index == current ? { transform: `scale(1.07)` } : null}
+    >
       <div>
-        <h3 className="text-black font-bold text-xl px-2 py-3">
+        <img
+          className="rounded-lg"
+          src={`/projects/project${index}.png`}
+          alt={project.title}
+        />
+        <h3 className="text-black font-bold text-2xl px-2 py-3">
           {project.title}
         </h3>
-        <p className=" text-4 px-2 mb-4 text-xs">{project.description}</p>
+        <p className=" text-4 px-2 mb-4 text-sm">{project.description}</p>
 
         {project?.technologies?.map((tech) => {
           return (
-            <span key={tech} className=" bg-black text-white p-2 m-1 rounded-full text-xs">
+            <span
+              key={tech}
+              className=" bg-black text-white p-2 m-1 rounded-full text-[10px]"
+            >
               {tech}
             </span>
           );
         })}
       </div>
       <div className="flex justify-evenly mt-4 mb-2">
-        <a className="bg-6 p-2 rounded-lg" href={project.repository}>CODE</a>
-        <a className="bg-1 py-2 px-5 rounded-lg" href={project.deploy}>Ir al proyecto</a>
+        <a className="bg-6 p-2 rounded-lg" href={project.repository}>
+          CODE
+        </a>
+        <a className="bg-1 py-2 px-5 rounded-lg" href={project.deploy}>
+          Ir al proyecto
+        </a>
       </div>
     </div>
   );
