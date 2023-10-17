41fd32220409c31911f1a3b960e03b717f71bbbb
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-10-17 15:22:54 -0300

    feat:created &&  added successAlert to contact form

--- src/components/AppComponents/ContactForm.jsx
+++ src/components/AppComponents/ContactForm.jsx
@@ -1,7 +1,11 @@
 "use client";
 import { useForm } from "react-hook-form";
+import SuccessAlert from "./SuccessAlert";
+import { useState } from "react";
 
 const ContactForm = () => {
+  const [success, setSuccess] = useState(false);
+
   const {
     register,
     handleSubmit,
@@ -22,102 +26,110 @@ const ContactForm = () => {
         .then((res) => console.log(res));
 
       reset();
+
+      setSuccess(true);
+      setTimeout(() => {
+        setSuccess(false);
+      }, 8000);
     } catch (error) {
       console.log(error);
     }
   });
 
   return (
-    <form
-      onSubmit={onSubmit}
-      className="flex flex-col border rounded-lg p-3 max-w-lg bg-black w-full"
-    >
-      <fieldset className="border rounded-lg px-3 mb-3">
-        <legend className="text-xs md:text-lg">
-          Nombre:{" "}
-          {errors.name && (
-            <span className=" text-red-500 text-xs md:text-sm">
-              {errors.name.message}
-            </span>
-          )}
-        </legend>
-        <input
-          className="bg-black w-full mb-2"
-          type="text"
-          {...register("name", {
-            required: {
-              value: true,
-              message: "es requerido",
-            },
-            minLength: {
-              value: 3,
-              message: "debe tener minimo tres caracteres",
-            },
-            maxLength: {
-              value: 20,
-              message: "debe tener maximo veinte caracteres",
-            },
-          })}
-        />
-      </fieldset>
+    <>
+      <form
+        onSubmit={onSubmit}
+        className="flex flex-col border rounded-lg p-3 max-w-lg bg-black w-full"
+      >
+        <fieldset className="border rounded-lg px-3 mb-3">
+          <legend className="text-xs md:text-lg">
+            Nombre:{" "}
+            {errors.name && (
+              <span className=" text-red-500 text-xs md:text-sm">
+                {errors.name.message}
+              </span>
+            )}
+          </legend>
+          <input
+            className="bg-black w-full mb-2"
+            type="text"
+            {...register("name", {
+              required: {
+                value: true,
+                message: "es requerido",
+              },
+              minLength: {
+                value: 3,
+                message: "debe tener minimo tres caracteres",
+              },
+              maxLength: {
+                value: 20,
+                message: "debe tener maximo veinte caracteres",
+              },
+            })}
+          />
+        </fieldset>
 
-      <fieldset className="border rounded-lg px-3 mb-3 ">
-        <legend className="text-xs md:text-lg">
-          Email:{" "}
-          {errors.email && (
-            <span className=" text-red-500 text-xs md:text-sm">
-              {errors.email.message}
-            </span>
-          )}
-        </legend>
-        <input
-          className="bg-black w-full mb-2"
-          type="email"
-          {...register("email", {
-            required: {
-              value: true,
-              message: "es requerido",
-            },
-            pattern: {
-              value: /^\S+@\S+\.\S+$/,
-              message: "no es valido",
-            },
-          })}
-        />
-      </fieldset>
+        <fieldset className="border rounded-lg px-3 mb-3 ">
+          <legend className="text-xs md:text-lg">
+            Email:{" "}
+            {errors.email && (
+              <span className=" text-red-500 text-xs md:text-sm">
+                {errors.email.message}
+              </span>
+            )}
+          </legend>
+          <input
+            className="bg-black w-full mb-2"
+            type="email"
+            {...register("email", {
+              required: {
+                value: true,
+                message: "es requerido",
+              },
+              pattern: {
+                value: /^\S+@\S+\.\S+$/,
+                message: "no es valido",
+              },
+            })}
+          />
+        </fieldset>
 
-      <fieldset className="border rounded-lg px-3 mb-3 ">
-        <legend className="text-xs md:text-lg">
-          Mensaje:{" "}
-          {errors.message && (
-            <span className=" text-red-500 text-xs md:text-sm">
-              {errors.message.message}
-            </span>
-          )}
-        </legend>
-        <textarea
-          className="bg-black w-full mb-2"
-          cols="30"
-          rows="8"
-          {...register("message", {
-            required: {
-              value: true,
-              message: "es requerido",
-            },
-            minLength: {
-              value: 10,
-              message: "Debe tener minimo diez caracteres",
-            },
-          })}
-        ></textarea>
-      </fieldset>
+        <fieldset className="border rounded-lg px-3 mb-3 ">
+          <legend className="text-xs md:text-lg">
+            Mensaje:{" "}
+            {errors.message && (
+              <span className=" text-red-500 text-xs md:text-sm">
+                {errors.message.message}
+              </span>
+            )}
+          </legend>
+          <textarea
+            className="bg-black w-full mb-2"
+            cols="30"
+            rows="8"
+            {...register("message", {
+              required: {
+                value: true,
+                message: "es requerido",
+              },
+              minLength: {
+                value: 10,
+                message: "Debe tener minimo diez caracteres",
+              },
+            })}
+          ></textarea>
+        </fieldset>
 
-      <fieldset className="flex justify-end py-2">
-        <button className="text-black text-center font-semibold text-sm bg-ea hover:bg-4 hover:text-white transition py-2 px-7 rounded-lg cursor-pointer">
-          Enviar
-        </button>
-      </fieldset>
-    </form>
+        <fieldset className="flex justify-end py-2">
+          <button className="text-black text-center font-semibold text-sm bg-ea hover:bg-4 hover:text-white transition py-2 px-7 rounded-lg cursor-pointer">
+            Enviar
+          </button>
+        </fieldset>
+      </form>
+      {success && <SuccessAlert/>}
+    </>
   );
 };
 
---
+++ src/components/AppComponents/SuccessAlert.jsx
@@ -0,0 +1,30 @@
+const SuccessAlert = () => {
+  return (
+    <dialog className="absolute flex items-start md:items-center top-20 bg-green-200 rounded-lg p-3 w-72 max-w-3xl mb-3 md:w-full animate-slideInUp">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="#41934d"
+        ariaHidden="true"
+        className="nz sb axw max-w-[25px] md:max-w-[30px]"
+      >
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd"
+        ></path>
+      </svg>
+      <div className="flex flex-col ml-1 md:ml-2">
+        <h3 className="text-base text-green-950 md:text-lg font-semibold">
+          Mensaje enviado con éxito!
+        </h3>
+        <p className="text-xs md:text-sm text-green-800 mt-1">
+          Se ha enviado un correo con información detallada a la dirección que
+          has proporcionado.
+        </p>
+      </div>
+    </dialog>
+  );
+};
+
+export default SuccessAlert;
