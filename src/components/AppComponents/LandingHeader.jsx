3e7d5a92227ee4ecf4b2f63267b7723dfb3a1122
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-10-17 15:21:29 -0300

    refactor: Landing header svg atributes

--- src/components/AppComponents/LandingHeader.jsx
+++ src/components/AppComponents/LandingHeader.jsx
@@ -20,11 +20,11 @@ const LandingHeader = () => {
             fill="none"
             stroke="#fafafa"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
               {" "}
@@ -43,20 +43,20 @@ const LandingHeader = () => {
             xmlns="http://www.w3.org/2000/svg"
             stroke="#fafafa"
           >
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
             <g
               id="SVGRepo_tracerCarrier"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
               {" "}
               <path
                 d="M4 6H20M4 12H20M4 18H20"
                 stroke="#fafafa"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
               ></path>{" "}
             </g>
           </svg>
