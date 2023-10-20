bbb1944575da8f458f4858b3515999ca25c7bf19
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-10-19 21:25:53 -0300

    feat: file to add constants options

---
+++ src/services/constants.js
@@ -0,0 +1,131 @@
+export const countries = [
+  {
+    id: "AR",
+    name: "Argentina",
+  },
+  {
+    id: "BR",
+    name: "Brasil",
+  },
+  {
+    id: "BO",
+    name: "Bolivia",
+  },
+  {
+    id: "CL",
+    name: "Chile",
+  },
+  {
+    id: "CO",
+    name: "Colombia",
+  },
+  {
+    id: "EC",
+    name: "Ecuador",
+  },
+  {
+    id: "ES",
+    name: "España",
+  },
+  {
+    id: "MX",
+    name: "México",
+  },
+  {
+    id: "PA",
+    name: "Panama",
+  },
+  {
+    id: "PY",
+    name: "Paraguay",
+  },
+  {
+    id: "PE",
+    name: "Perú",
+  },
+  {
+    id: "UY",
+    name: "Uruguay",
+  },
+  {
+    id: "US",
+    name: "USA",
+  },
+  {
+    id: "VE",
+    name: "Venezuela",
+  },
+  {
+    id: "Otro",
+    name: "Otro",
+  },
+];
+
+export const itKnowledge = [
+  {
+    id: "1/5",
+    description: "Absolutamente nada",
+  },
+  {
+    id: "2/5",
+    description: "He visto cositas",
+  },
+  {
+    id: "3/5",
+    description: "Estoy aprendiendo",
+  },
+  {
+    id: "4/5",
+    description: "Soy programador",
+  },
+  {
+    id: "5/5",
+    description: "Tengo mucha experiencia",
+  },
+];
+
+export const satisfactionOptions = [
+  {
+    id: "MS",
+    description: "Muy satisfactorio",
+  },
+  {
+    id: "S",
+    description: "Satisfactorio",
+  },
+  {
+    id: "N",
+    description: "Neutral",
+  },
+  {
+    id: "I",
+    description: "nsatisfactorio",
+  },
+  {
+    id: "MI",
+    description: "Muy insatisfactorio",
+  },
+];
+
+export const calificationOptions = [
+  {
+    id: "MB",
+    description: "Muy bueno",
+  },
+  {
+    id: "B",
+    description: "Bueno",
+  },
+  {
+    id: "A",
+    description: "Aceptable",
+  },
+  {
+    id: "NM",
+    description: "Necesita mejoras",
+  },
+  {
+    id: "P",
+    description: "Pobre",
+  },
+];
